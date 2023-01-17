165b7e3efaa3780d41c82ea3d6b53b8e0008769b
Pena <gui.domenes@gmail.com>
2023-01-17 15:54:07 -0300

    front 17/01

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,75 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="row">
+      <div class="card" style="" v-for="text in textos" :key="text.id">
+        <div class="img-tamanho">
+        <img class="card-img-top" :src="text.img" alt="Imagem de capa do card" />
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">{{ text.nome }}</h5>
+          <p class="card-text">
+            {{ text.descricao }}
+          </p>
+          <button class="btn btn-primary" @click="excluir(text.id)">Visitar</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import api from "@/services/http.js";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "HelloWorld",
+  data() {
+    return {
+      textos: [],
+    };
+  },
+  created() {
+    api.get("produtos").then((res) => {
+      this.textos = res.data;
+      console.log(res.data);
+    });
+  },
+  methods: {
+    excluir(id){
+      api.delete(`produtos/${id}`).then(res => {
+        console.log(res);
+      })
+    }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.card {
+  display: flex;
+  max-width: 20%;
+  padding: 1em;
+  margin: 1.5em;
+  border-radius: 0.25rem;
+  box-shadow: 2px 3px 10px #5e5e5e;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.btn {
+  position: absolute;
+  bottom: 15px;
+  left: 35%;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.card-text {
+  margin-bottom: 3em;
 }
-a {
-  color: #42b983;
+
+.img-tamanho {
+    height: 200px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.card-img-top {
+  width: 190px;
 }
 </style>
